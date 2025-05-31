e3da3f4a7d51dd636633b732062bcf938b763232
Daniel120904 <danieladriano1209@gmail.com>
2025-05-31 14:05:51 -0300

    Estrutura inicial do projeto, cabeçalho e componentes de social media

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { SocialMediaComponents } from './data.js'
 
-function App() {
-  const [count, setCount] = useState(0)
+const headerDescriptions = [
+  'mim!',
+  'minha história!',
+  'minhas habilidades!',
+  'meus estudos!',
+]
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
+function Header() {
+  const headerDescription = headerDescriptions[genRandomInt(3)]
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <header>
+      <h1>Meu Blog</h1>
+      <p>Um pouco sobre {headerDescription}</p>
+    </header>
+  )
+}
+
+function SocialMedia({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="social-media">
+          <h2>Minhas redes sociais e contatos</h2>
+          <ul>
+            <SocialMedia
+              image={SocialMediaComponents[0].image}
+              title={SocialMediaComponents[0].title}
+              description={SocialMediaComponents[0].description}
+            />
+            <SocialMedia {...SocialMediaComponents[1]} />
+            <SocialMedia {...SocialMediaComponents[2]} />
+            <SocialMedia {...SocialMediaComponents[3]} />
+          </ul>
+        </section>
+      </main>
+    </div>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,27 @@
+import linkedinImg from './assets/logoLinkedin.png'
+import gitHubImg from './assets/logoGitHub.png'
+import whatsAppImg from './assets/logoWhatsApp.png'
+import emailImg from './assets/logoEmail.png'
+
+export const SocialMediaComponents = [
+  {
+    image: linkedinImg,
+    title: 'Linkedin',
+    description: 'Perfil no Linkedin:\nlinkedin.com/in/daniel-adriano-02b66732b',
+  },
+  {
+    image: gitHubImg,
+    title: 'GitHub',
+    description: 'Perfil no GitHub:\nDaniel120904',
+  },
+  {
+    image: whatsAppImg,
+    title: 'Telefone',
+    description: 'Número de telefone:\n11 91401-2544',
+  },
+  {
+    image: emailImg,
+    title: 'E-mail',
+    description: 'Meu E-mail privado:\ndanieladriano1209@gmail.com',
+  },
+]
